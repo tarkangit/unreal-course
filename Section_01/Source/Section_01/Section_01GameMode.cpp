ca5513e0d135a02303463103090a1f5b3f710d54
tarkangit <tarkangit>
2016-04-05 16:30:22 +0300

    Committed code of course section 1.

---
+++ Section_01/Source/Section_01/Section_01GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Section_01.h"
+#include "Section_01GameMode.h"
+
+
+
+
---
+++ Section_01/Source/Section_01/Section_01GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Section_01GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION_01_API ASection_01GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
